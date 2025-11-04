c5ddf6da1a876e98d3fe213da70c064b2e39cde2
xiaojianzheng <1272209235@qq.com>
2025-11-04 16:53:02 +0800

    feat(main): 增加d.ts类型文件

--- src/global.d.ts
+++
@@ -1,6 +0,0 @@
-declare global {
-  var LOCAL_PLUGINS: any;
-  var OP_CONFIG: any;
-}
-
-export {};
---
+++ src/types/global.d.ts
@@ -0,0 +1,46 @@
+interface FileInfo {
+  /** 是否为文件 */
+  isFile: boolean;
+  /** 是否为文件夹 */
+  isDirectory: boolean;
+  /** 文件名（含后缀） */
+  name: string;
+  /** 文件绝对路径 */
+  path: string;
+}
+
+interface Cmd {
+  label: string;
+  type?: 'text' | 'img' | 'over' | 'file' | 'files' | 'regex';
+  fileType: string;
+  match: CmdMatch;
+}
+
+interface CmdMatch {
+  app: string[];
+  class: string[];
+}
+
+interface Feature {
+  code: string;
+  explain: string;
+  cmds: Array<Cmd | string>;
+}
+
+interface Option {
+  name: string;
+  value: string;
+  icon: string;
+  desc: string;
+  type: string;
+  match: [number, number] | false;
+  zIndex: number;
+  click: () => void;
+}
+
+// declare global {
+//   var LOCAL_PLUGINS: any;
+//   var OP_CONFIG: any;
+// }
+//
+// export {};
