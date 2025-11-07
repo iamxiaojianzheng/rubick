93c1bf44d47f47d594c95a7c3ebe0fc665735b97
xiaojianzheng <1272209235@qq.com>
2025-11-07 14:40:33 +0800

    feat(main): 优化Windows环境下App检索

--- src/core/app-search/win.ts
+++ src/core/app-search/win.ts
@@ -10,77 +10,70 @@ const appData = path.join(os.homedir(), './AppData/Roaming');
 const startMenu = path.join(appData, 'Microsoft\\Windows\\Start Menu\\Programs');
 
 const fileLists: any = [];
-const isZhRegex = /[\u4e00-\u9fa5]/;
+const splitNameRegex = /[ _-]/;
 
-const icondir = path.join(os.tmpdir(), 'ProcessIcon');
-const exists = fs.existsSync(icondir);
+const iconDir = path.join(os.tmpdir(), 'ProcessIcon');
+const exists = fs.existsSync(iconDir);
 if (!exists) {
-  fs.mkdirSync(icondir);
+  fs.mkdirSync(iconDir);
 }
 
-const getico = (app) => {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const fileIcon = require('extract-file-icon');
-    const buffer = fileIcon(app.desc, 32);
-    const iconpath = path.join(icondir, `${app.name}.png`);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fileIcon = require('extract-file-icon');
 
-    fs.exists(iconpath, (exists) => {
-      if (!exists) {
-        fs.writeFile(iconpath, buffer, 'base64', () => {
-          //
-        });
-      }
-    });
+const getico = (iconPath: string, originFile: string, target: string) => {
+  try {
+    const buffer = fileIcon(originFile, 32) || fileIcon(target, 32);
+    if (buffer) {
+      fs.writeFile(iconPath, buffer, 'base64', () => {
+        return;
+      });
+    }
   } catch (e) {
-    console.log(e, app.desc);
+    console.log(e, target);
   }
 };
 
-function fileDisplay(filePath) {
+function fileDisplay(filePath: string) {
   //根据文件路径读取文件，返回文件列表
-  fs.readdir(filePath, function (err, files) {
+  fs.readdir(filePath, function (err, fileNames) {
     if (err) {
       console.warn(err);
     } else {
-      files.forEach(function (filename) {
-        const filedir = path.join(filePath, filename);
-        fs.stat(filedir, function (eror, stats) {
+      fileNames.forEach(function (fileName) {
+        const fullPath = path.join(filePath, fileName);
+        fs.stat(fullPath, function (eror, stats) {
           if (eror) {
             console.warn('获取文件stats失败');
           } else {
-            const isFile = stats.isFile(); // 是文件
             const isDir = stats.isDirectory(); // 是文件夹
+            if (isDir) {
+              fileDisplay(fullPath); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+            }
+
+            const isFile = stats.isFile(); // 是文件
             if (isFile) {
-              const appName = filename.split('.')[0];
+              const appName = fileName.split('.')[0];
               const keyWords = [appName];
               let appDetail: any = {};
               try {
-                appDetail = shell.readShortcutLink(filedir);
+                appDetail = shell.readShortcutLink(fullPath);
+                // console.log(appDetail);
               } catch (e) {
                 //
               }
-              if (!appDetail.target || appDetail.target.toLowerCase().indexOf('unin') >= 0) return;
+
+              if (!appDetail.target) return;
 
               // C:/program/cmd.exe => cmd
-              keyWords.push(path.basename(appDetail.target, '.exe'));
+              keyWords.push(fileName);
 
-              if (isZhRegex.test(appName)) {
-                // const [, pinyinArr] = translate(appName);
-                // const zh_firstLatter = pinyinArr.map((py) => py[0]);
-                // // 拼音
-                // keyWords.push(pinyinArr.join(''));
-                // 缩写
-                // keyWords.push(zh_firstLatter.join(''));
-              } else {
-                const firstLatter = appName
-                  .split(' ')
-                  .map((name) => name[0])
-                  .join('');
-                keyWords.push(firstLatter);
+              if (splitNameRegex.test(appName)) {
+                keyWords.push(...appName.split(splitNameRegex));
               }
 
-              const icon = path.join(os.tmpdir(), 'ProcessIcon', `${encodeURIComponent(appName)}.png`);
+              // const icon = path.join(iconDir, `${encodeURIComponent(appName)}.png`);
+              const icon = path.join(iconDir, `${appName}.png`);
 
               const appInfo = {
                 value: 'plugin',
@@ -88,16 +81,13 @@ function fileDisplay(filePath) {
                 type: 'app',
                 icon,
                 pluginType: 'app',
-                action: `start "dummyclient" "${appDetail.target}"`,
+                action: `${appDetail.target} ${appDetail.args}`,
                 keyWords: keyWords,
                 name: appName,
                 names: JSON.parse(JSON.stringify(keyWords)),
               };
               fileLists.push(appInfo);
-              getico(appInfo);
-            }
-            if (isDir) {
-              fileDisplay(filedir); // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
+              getico(icon, fullPath, appDetail.target);
             }
           }
         });
@@ -109,5 +99,6 @@ function fileDisplay(filePath) {
 export default () => {
   fileDisplay(filePath);
   fileDisplay(startMenu);
+  console.log(fileLists);
   return fileLists;
 };
